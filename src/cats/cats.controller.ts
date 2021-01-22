237f04f1dd10932c1dd64cce5de8cba29aa8d8ec
HungTran <hungdng.92@gmail.com>
2021-01-22 17:58:23 +0700

    init

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,29 @@
+import {
+  Controller,
+  Get,
+  Query,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private catsService: CatsService) {}
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+}
---
+++ src/cats/dto/index.ts
@@ -0,0 +1,4 @@
+import { CreateCatDto } from './create-cat.dto';
+import { UpdateCatDto } from './update-cat.dto';
+import { ListAllEntities } from './list-cat.dto';
+export { CreateCatDto, UpdateCatDto, ListAllEntities };
